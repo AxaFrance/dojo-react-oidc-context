cc78111fb77cb5fc1879a98a74ac9badd9fc4662
Olivier YOUF <olivier.youf@axa.fr>
2019-01-04 10:53:53 +0100

    Correction part 3

--- exercice-sample/src/Pages/Home/Home.container.js
+++ exercice-sample/src/Pages/Home/Home.container.js
@@ -1,3 +1,16 @@
-import Component from "./Home";
+import Component from './Home';
+import { withProps, compose } from 'recompose';
+import { withOidcUser } from '@axa-fr/react-oidc-context';
 
-export default Component;
+const setOidcUserProps = withProps(({ oidcUser }) => ({
+  connectedUserFullName: oidcUser
+    ? `${oidcUser.profile.given_name} ${oidcUser.profile.family_name}`
+    : undefined,
+}));
+
+const enhance = compose(
+  withOidcUser,
+  setOidcUserProps,
+);
+
+export default enhance(Component);
